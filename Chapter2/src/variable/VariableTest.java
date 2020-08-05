253ff05f9713c6fcca0535ffbed44011ff93b3a9
Byeongseung <qudtmd95@naver.com>
2020-08-05 22:17:39 +0900

    Chapter2

---
+++ Chapter2/src/variable/VariableTest.java
@@ -0,0 +1,16 @@
+package variable;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+
+		int age, count;
+		
+		age = 40;
+		
+		System.out.println(age);
+		
+		int numberOfStudent;
+	}
+
+}
