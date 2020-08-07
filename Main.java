ff40efcf58f5992bfb138b3af08e605176a5334e
Kimbyeongseung <64355773+Kimbyeongseung@users.noreply.github.com>
2020-08-07 17:15:04 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String input = "";
+		int kor = 100;
+		int eng = 100;
+		int math = 100;
+		int sci =100;
+		
+		System.out.println("과목 입력");
+		input = scan.nextLine();
+		
+		if(input.equals("국어")) {
+			//현재 국어 점수를 보여줌
+			//새로운 국어 점수를 입력 받음
+			// kor 변수에 새로 입력 받은 점수를 넣어줌		
+		}else if(input.equals("영어")) {
+			//현재 영어 점수를 보여줌
+			//새로운 영어 점수를 입력 받음
+			// eng 변수에 새로 입력 받은 점수를 넣어줌
+		}
+
+		
+		
+		
+		//총점과 평균 보여주기
+		
+		
+
+	}
+
+}
