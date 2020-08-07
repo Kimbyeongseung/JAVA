41236579c8c5a1de2d420375754fd7acf2b04e10
Kimbyeongseung <64355773+Kimbyeongseung@users.noreply.github.com>
2020-08-07 15:05:21 +0900

    if 조건문

---
+++ TestVersion2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class TestVersion2 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan =new Scanner(System.in);
+		
+		int a;
+		
+		String b;
+		
+		b = scan.nextLine();
+		System.out.println(b);
+		
+		a = 0;
+		a = a+1;
+		System.out.println(a + 1 + "번 째 입력입니다.");
+		b = scan.nextLine();
+		System.out.println(b);
+		
+		
+
+
+		
+	}
+	
+	
+}
