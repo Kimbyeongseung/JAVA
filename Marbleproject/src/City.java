e73a3ef7283b49c3c79e710915dccb93f3b19a7b
Kimbyeongseung <64355773+Kimbyeongseung@users.noreply.github.com>
2020-08-17 18:56:17 +0900

    Marble Project

---
+++ Marbleproject/src/City.java
@@ -0,0 +1,12 @@
+
+public class City { //게임판 움직임과 정보
+
+	int isUp; //상              0 = 없다   1= 출발 2 = 황금열쇠  3 = 우주여행  4 = 사회복지기금  5 = 무인도  
+	int isDown; //하
+	int isLeft;	//좌
+	int isRight; //우
+	
+	int eventType;
+	
+	
+}
---
+++ Marbleproject/src/Marble.java
@@ -0,0 +1,10 @@
+
+public class Marble {
+
+	public static void main(String[] args) {
+
+		City map = new City();
+		
+	}
+
+}
